744c4dd9cce6de2203c89236a0f16c5ce69ab30b
OgurecRoman <treninasweets@gmail.com>
2025-11-10 15:05:35 +0300

    another one try...

--- server/functions/check_event.js
+++ server/functions/check_event.js
@@ -2,7 +2,7 @@ import prisma from "../lib/prisma";
 
 const ONE_DAY_MS = 24 * 60 * 60 * 1000; 
 
-export default async (req, res) => {
+export default async function checkEventync(req, res){
   console.log("Начиваем проверку событий......");
   const now = new Date();
   
