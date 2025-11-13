278e9b8d94c308e4ec313d0eeb3e6aa21e5e0699
OgurecRoman <treninasweets@gmail.com>
2025-11-13 09:55:51 +0300

    add max ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { MaxUI } from '@maxhub/max-ui';
+import '@maxhub/max-ui/dist/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Старый способ рендера для React < 18
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MaxUI>
+        <App />
+  </MaxUI>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
