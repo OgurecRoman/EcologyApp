1e382780f6e62f910c08ad7db96c6f505dc94796
OgurecRoman <treninasweets@gmail.com>
2025-11-13 22:21:46 +0300

    fix...

--- client/src/api/eventsApi.js
+++ client/src/api/eventsApi.js
@@ -2,6 +2,9 @@ import axios from "axios"
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api"
 
+console.log("API")
+console.log(API_URL)
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
