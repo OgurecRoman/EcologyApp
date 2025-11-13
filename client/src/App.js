81aab0b7f98c4a8158a02a4d83e9d1746228b3f8
OgurecRoman <treninasweets@gmail.com>
2025-11-13 03:51:10 +0300

    remove unused

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MapTab from './components/MapTab';
 import AddEventTab from './components/AddEventTab';
 import Tabs from './components/Tabs';
