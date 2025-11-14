1f4c866327212909a1455f04cb66782e80434709
OgurecRoman <treninasweets@gmail.com>
2025-11-15 01:24:51 +0300

    fix prisma

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-
-const connectionString = process.env.DATABASE_URL; 
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
-
+  
+const globalForPrisma = global;  
+const prisma = globalForPrisma.prisma || new PrismaClient();  
+  
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+  
 export default prisma;
-
---
+++ server/prisma/migrations/20251114220535_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Event" ADD COLUMN     "actual" BOOLEAN NOT NULL DEFAULT true;
