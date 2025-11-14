fd094a46965b36870bd590c85cc9c4069b89b3ff
Klenikov <klenikoff.pavel@yandex.ru>
2025-11-15 00:59:47 +0300

    docker and ostalnoe

--- server/api/cron-job.js
+++ server/api/cron-job.js
@@ -1,24 +1,24 @@
 import prisma from "../lib/prisma.js";
-
 const ONE_DAY_MS = 24 * 60 * 60 * 1000;
-
 export default async function (req, res) {
+    if (req.method !== 'GET') {
+        return res.status(405).send('Method Not Allowed');
+    }
     console.log("Начинаем проверку событий......");
     const now = new Date();
-
-    const futureEvents = new Date(now.getTime() + ONE_DAY_MS);
-
+    const tomorrowStart = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+    const dayAfterStart = new Date(tomorrowStart.getTime() + ONE_DAY_MS);
     try {
         const eventsToNotify = await prisma.event.findMany({
             where: {
                 date: {
-                    lte: futureEvents
+                    gte: tomorrowStart,
+                    lt: dayAfterStart
                 },
                 isNotified: false,
             },
             include: { participants: true }
         });
-
         for (const event of eventsToNotify) {
             for (const user of event.participants){
                 console.log(`[НАПОМИНАНИЕ]: Отправил напоминание для события ID: ${event.id} пользователю ${user.name}. Дата: ${event.date.toISOString()}`);
@@ -28,9 +28,7 @@ export default async function (req, res) {
                 data: { isNotified: true }
             });
         }
-
         res.status(200).send('Cron Job выполнен успешно.');
-
     } catch (error) {
         console.error('Ошибка Cron Job:', error);
         res.status(500).send('Ошибка Cron Job.');
