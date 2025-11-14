5414d8af8ce0c646c921defe9c6ae0b4ce76bda4
OgurecRoman <treninasweets@gmail.com>
2025-11-15 01:41:55 +0300

    fix migrations

--- server/prisma/migrations/20251114220535_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "actual" BOOLEAN NOT NULL DEFAULT true;
--- server/prisma/migrations/20251114223533_init/migration.sql
+++ server/prisma/migrations/20251114223533_init/migration.sql
@@ -1,6 +1,22 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "lastActivity" TIMESTAMP(3),
-ADD COLUMN     "rating" INTEGER NOT NULL DEFAULT 0;
+-- CreateEnum
+CREATE TYPE "EventType" AS ENUM ('SUBBOTNIK', 'PAPER_COLLECTION', 'BATTERY_COLLECTION', 'PLASTIC_COLLECTION', 'GLASS_COLLECTION', 'ELECTRONICS_COLLECTION', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "type" "EventType" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "address" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "isNotificate" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "actual" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "Post" (
@@ -16,12 +32,31 @@ CREATE TABLE "Post" (
 );
 
 -- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL,
+    "username" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL DEFAULT 0,
+    "lastActivity" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Like" (
     "id" SERIAL NOT NULL,
     "userId" INTEGER NOT NULL,
     "postId" INTEGER NOT NULL,
 
     CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_EventToUser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_EventToUser_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateTable
@@ -36,6 +71,9 @@ CREATE TABLE "_UserFollows" (
 CREATE UNIQUE INDEX "Like_userId_postId_key" ON "Like"("userId", "postId");
 
 -- CreateIndex
+CREATE INDEX "_EventToUser_B_index" ON "_EventToUser"("B");
+
+-- CreateIndex
 CREATE INDEX "_UserFollows_B_index" ON "_UserFollows"("B");
 
 -- AddForeignKey
@@ -46,6 +84,12 @@ ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFE
 
 -- AddForeignKey
 ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventToUser" ADD CONSTRAINT "_EventToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_EventToUser" ADD CONSTRAINT "_EventToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_UserFollows" ADD CONSTRAINT "_UserFollows_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
